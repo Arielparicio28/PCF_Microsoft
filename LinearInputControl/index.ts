e3a015cbbd72654ac9f7db1650889c5d5f103908
Arielparicio28 <arielaparicio100@gmail.com>
2024-05-29 19:12:38 +0200

    Añadiendo input y labels,etc. en mi archivo index.ts

--- LinearInputControl/index.ts
+++ LinearInputControl/index.ts
@@ -1,53 +1,141 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
-export class LinearInputControl implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+export class LinearInputControl
+  implements ComponentFramework.StandardControl<IInputs, IOutputs>
+{
+  //Variables privadas que solo estaran accesibles dentro de la misma clase.
 
-    /**
-     * Empty constructor.
-     */
-    constructor()
-    {
+  //Descripción: Esta variable almacenará el valor actual del control. En este caso, se espera que sea un número.
+  //Uso: Se utiliza para almacenar el valor interno del control, posiblemente el valor introducido por el usuario o establecido por el contexto.
+  private _value: number;
 
-    }
+  //Descripción:Esta variable es una función que se llama para notificar al framework que el valor de salida del control ha cambiado.
+  //Uso: Se usa para informar al framework que el valor del control ha cambiado, lo cual puede desencadenar actualizaciones en otras partes de la aplicación.
+  private _notifyOutputChanged: () => void;
 
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-     */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
-        // Add control initialization code
-    }
+  //Descripción:Esta variable representa un elemento HTML de etiqueta (<label>).
+  //Uso: Se utiliza para mostrar una etiqueta descriptiva asociada con el control de entrada. Es útil para la accesibilidad y para proporcionar contexto al usuario sobre lo que se espera que ingresen en el control.
+  private labelElement: HTMLLabelElement;
 
+  //Descripción: Esta variable representa un elemento HTML de entrada (<input>).
+  //Uso: Se utiliza para capturar la entrada del usuario. Este es el campo donde el usuario ingresará datos que serán gestionados por el control.
+  private inputElement: HTMLInputElement;
 
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
-    {
-        // Add code to update control view
-    }
+  //Descripción: Esta variable representa un elemento HTML de contenedor (<div>).
+  //Uso: Se utiliza como el contenedor principal para el control. Dentro de este contenedor se pueden colocar otros elementos HTML como el labelElement y inputElement.
+  private _container: HTMLDivElement;
 
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
-     */
-    public getOutputs(): IOutputs
-    {
-        return {};
-    }
+  //Descripción: Esta variable contiene el contexto del framework que proporciona información y servicios a los controles.
+  //Uso: Se utiliza para interactuar con el framework de PowerApps, obteniendo datos de entrada, servicios y otra información necesaria para el funcionamiento del control.
+  private _context: ComponentFramework.Context<IInputs>;
 
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
-    public destroy(): void
-    {
-        // Add code to cleanup control if necessary
-    }
+  //Descripción: Esta variable es un listener de eventos que se usa para manejar eventos que requieren que el control se actualice.
+  //Uso: Se utiliza para definir una función que se ejecutará cuando ocurra un evento específico (por ejemplo, un cambio en el valor del campo de entrada), permitiendo que el control responda dinámicamente a las interacciones del usuario.
+  private _refreshData: EventListenerOrEventListenerObject;
+
+  /**
+   * Empty constructor.
+   */
+  constructor() {}
+
+  /**
+   * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+   * Data-set values are not initialized here, use updateView.
+   * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+   * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+   * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+   * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+   */
+  public init(
+    context: ComponentFramework.Context<IInputs>,
+    notifyOutputChanged: () => void,
+    state: ComponentFramework.Dictionary,
+    container: HTMLDivElement
+  ): void {
+    // Add control initialization code
+    this._context = context;
+    this._container = document.createElement("div");
+    this._notifyOutputChanged = notifyOutputChanged;
+    this._refreshData = this.refreshData.bind(this);
+
+    // creating HTML elements for the input type range and binding it to the function which
+    // refreshes the control data
+    this.inputElement = document.createElement("input");
+    this.inputElement.setAttribute("type", "range");
+    this.inputElement.addEventListener("input", this._refreshData);
+
+    //setting the max and min values for the control.
+    this.inputElement.setAttribute("min", "1");
+    this.inputElement.setAttribute("max", "1000");
+    this.inputElement.setAttribute("class", "linearslider");
+    this.inputElement.setAttribute("id", "linearrangeinput");
+
+    // creating a HTML label element that shows the value that is set on the linear range control
+    this.labelElement = document.createElement("label");
+    this.labelElement.setAttribute("class", "LinearRangeLabel");
+    this.labelElement.setAttribute("id", "lrclabel");
+
+    // retrieving the latest value from the control and setting it to the HTMl elements.
+    this._value = context.parameters.controlValue.raw!;
+    this.inputElement.setAttribute(
+      "value",
+      context.parameters.controlValue.formatted
+        ? context.parameters.controlValue.formatted
+        : "0"
+    );
+    this.labelElement.innerHTML = context.parameters.controlValue.formatted
+      ? context.parameters.controlValue.formatted
+      : "0";
+
+    // appending the HTML elements to the control's HTML container element.
+    this._container.appendChild(this.inputElement);
+    this._container.appendChild(this.labelElement);
+    container.appendChild(this._container);
+  }
+
+  /**
+   * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+   * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+   */
+  public updateView(context: ComponentFramework.Context<IInputs>): void {
+    // Add code to update control view
+    // storing the latest context from the control.
+    this._value = context.parameters.controlValue.raw!;
+    this._context = context;
+    this.inputElement.setAttribute(
+      "value",
+      context.parameters.controlValue.formatted
+        ? context.parameters.controlValue.formatted
+        : ""
+    );
+    this.labelElement.innerHTML = context.parameters.controlValue.formatted
+      ? context.parameters.controlValue.formatted
+      : "";
+  }
+
+  /**
+   * It is called by the framework prior to a control receiving new data.
+   * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
+   */
+  public getOutputs(): IOutputs {
+    return {
+      controlValue: this._value,
+    };
+  }
+
+  /**
+   * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+   * i.e. cancelling any pending remote calls, removing listeners, etc.
+   */
+  public destroy(): void {
+    // Add code to cleanup control if necessary
+    this.inputElement.removeEventListener("input", this._refreshData);
+  }
+
+  //Cree esta función para controlar eventos
+  public refreshData(evt: Event): void {
+    this._value = this.inputElement.value as any as number;
+    this.labelElement.innerHTML = this.inputElement.value;
+    this._notifyOutputChanged();
+  }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -43,3 +43,8 @@ Cambiado a: `<resources> <code path="index.ts" order="1"/> <css path="css/Linear
 Así agregue el elemento css.
 
 7. Generar el archivo `ManifestTypes.d.ts` con el siguiente comando. `npm run refreshTypes`
+
+
+8. Implementación de la lógica " El siguiente paso después de implementar el archivo de manifiesto es implementar la lógica del componente mediante TypeScript." la lógica se implementa en el archivo `index.ts`
+
+9. Definí variables privadas en el arcivo `index.ts`, estas variables privadas permiten a la clase LinearInputControl manejar su estado interno, interactuar con el DOM para mostrar y capturar datos del usuario, y comunicarse con el framework de PowerApps para integrarse correctamente en una aplicación de PowerApps.
